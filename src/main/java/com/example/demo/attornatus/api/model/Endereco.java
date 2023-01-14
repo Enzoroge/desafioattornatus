ddcb3eead2bca3decff2752e5d4d3ee9d6c9e2fd
Rogerio <rogeriolima20042000@yahoo.com.br>
2023-01-13 22:24:10 -0300

    criado classe Pessoas e Endereco e acesso ao banco h2

---
+++ src/main/java/com/example/demo/attornatus/api/model/Endereco.java
@@ -0,0 +1,41 @@
+package com.example.demo.attornatus.api.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+	
+	private String logradouro;
+	private String cep;
+	private String numero;
+	private String cidade;
+	
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+		
+
+}
---
+++ src/main/java/com/example/demo/attornatus/api/model/Pessoas.java
@@ -0,0 +1,56 @@
+package com.example.demo.attornatus.api.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
+public class Pessoas {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	private LocalDate dataNascimento;
+	
+	@Embedded
+	private Endereco endereco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	
+
+}
